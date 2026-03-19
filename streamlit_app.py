726764825e62414c750e28014ba49f27980525f7
Sobarinetech <sobarinetechno@gmail.com>
2026-03-19 21:40:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(
     layout="wide",
 )
 
-API_URL = "https://sqwevzxsufrbsyjrucuw.supabase.co/functions/v1/verify-prescription-api"
+API_URL = "https://rcbonfsszfmpiacinysg.supabase.co/functions/v1/verify-prescription-api"
 
 # ── Helpers ───────────────────────────────────────────────────────────────────
 
